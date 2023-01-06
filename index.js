5ad804305be49036c9c4d5b656492c28e1d9db27
agarwalayush9 <agarwalayush991@gmail.com>
2023-01-06 22:31:24 +0530

    updated

--- index.js
+++ index.js
@@ -4,18 +4,73 @@ let postfix = document.getElementById('post');
 let out = document.getElementById('out');
 let output = document.getElementById('output');
 let input = document.getElementById('input');
+
 function pre() {
     postfix.style.backgroundColor = 'rgba(5, 97, 154, 0.422)';
     prefix.style.backgroundColor = '#faae9773';
     out.innerHTML = "Prefix Expression :";
-    const data1 = [];
-    let size1 = data1.length;
+    const data = [];
+    let size = data.length;
     if (input.value != "") {
         output.innerHTML = "";
+        let val= input.value;
+        let rev=val.split('').reverse().join('');
+        let infix = Array.from(rev);
+        let j=0;
+        let res="";
+        while(infix[j] != undefined){
+            if(infix[j]=='('){
+                infix[j]=')';
+                j++;
+            }
+            else if(infix[j]==')'){
+                infix[j] ='(';
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        // console.log(infix);
+        let i=0;
+        while(infix[i]!= undefined){
+            if (!operator(infix[i])) {
+                res += infix[i];
+                i++;
+            }
+            else if(infix[i]=='('){
+                data[size]=infix[i];
+                size++;
+                i++;
+            }
+            else if(infix[i]==')'){
+                while(data[size-1]!='('){
+                    res+= data[size-1];
+                    size--;
+                }
+                size--;
+                i++;
+            }
+            else if(precedence(infix[i])> precedence(data[size-1])){
+                data[size]=infix[i];
+                size++;
+                i++;
+            }
+            else{
+                res+= data[size-1];
+                size--;
+            }
+        }
+        while(size>0){
+            res+=data[size-1];
+            size--;
+        }
+        output.innerHTML=res.split('').reverse().join('');
     }
     else {
         output.innerHTML = "No Input";
     }
+
 }
 
 function post() {
@@ -31,6 +86,19 @@ function post() {
         while(infix[i] !=undefined){
             if (!operator(infix[i])) {
                 output.innerHTML += infix[i];
+                i++;
+            }
+            else if(infix[i]=='('){
+                data[size]=infix[i];
+                size++;
+                i++;
+            }
+            else if(infix[i]==')'){
+                while(data[size-1]!='('){
+                    output.innerHTML+= data[size-1];
+                    size--;
+                }
+                size--;
                 i++;
             }
             else if(precedence(infix[i])> precedence(data[size-1])){
@@ -63,7 +131,7 @@ function precedence(ch) {
 }
 
 function operator(ch) {
-    if (ch == '*' || ch == '/' || ch == '+' || ch == '-')
+    if (ch == '*' || ch == '/' || ch == '+' || ch == '-'|| ch== '(' || ch==')')
         return 1;
     else
         return 0;
